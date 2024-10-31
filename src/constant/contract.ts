ff03b88d121909e45bfdf57852e91efedbf4120b
invf <115990356+invf@users.noreply.github.com>
2024-10-31 19:45:32 +0200

    Change description app

--- src/constant/contract.ts
+++ src/constant/contract.ts
@@ -1,16 +1,17 @@
 export const CONTRACT = {
-  name: 'Snoppy',
-  symbol: 'SNPY',
+  name: 'Hermes',
+  symbol: 'Hermes',
   ca: '0x0000000000000000000000000000000000000000',
   totalSupply: 1000000000,
   taxPercentage: 5,
   ownershipRenounced: true,
   description:
-    'Snoppy is a community made and owned Crypto currency project. We are aspiring to become the new high quality standard within the Crypto Space. A group of like minded individuals have come together to create the new golden standard of what a community should have to offer. We have a 5% tax on both buy and sell orders. We are in no way affiliated with Baby or the Baby family. However, we will be donating 5% of our marketing budget to the Baby campaign.',
+    'Hermes is an innovative application designed to help users earn money through cryptocurrency trading on the ByBit exchange. With a user-friendly interface and advanced trading tools, Hermes enables both novice and experienced traders to navigate the dynamic world of cryptocurrencies with ease.'
+,
   partners: [
     {
-      name: 'PinkSale',
-      url: 'https://www.pinksale.finance',
+      name: 'ByBit',
+      url: 'https://www.bybit.com/en/',
       imgUrl: '/pinksale.png',
     },
     { name: 'DEXView', url: 'https://www.dexview.com', imgUrl: '/dexview.svg' },
