aaddee8e7c9ddd4f92af5837e659d8f2ae6c4457
invf <115990356+invf@users.noreply.github.com>
2024-11-01 18:34:38 +0200

    feat(branding): update favicon and logo to align with the new branding guidelines

--- src/constant/contract.ts
+++ src/constant/contract.ts
@@ -1,13 +1,12 @@
 export const CONTRACT = {
-  name: 'Hermes',
+  name: 'Hermes Finance',
   symbol: 'Hermes',
   ca: '0x0000000000000000000000000000000000000000',
   totalSupply: 1000000000,
   taxPercentage: 5,
   ownershipRenounced: true,
   description:
-    'Hermes is an innovative application designed to help users earn money through cryptocurrency trading on the ByBit exchange. With a user-friendly interface and advanced trading tools, Hermes enables both novice and experienced traders to navigate the dynamic world of cryptocurrencies with ease.'
-,
+    'Hermes is an innovative application designed to help users earn money through cryptocurrency trading on the ByBit exchange. With a user-friendly interface and advanced trading tools, Hermes enables both novice and experienced traders to navigate the dynamic world of cryptocurrencies with ease.',
   partners: [
     {
       name: 'ByBit',
@@ -20,7 +19,7 @@ export const CONTRACT = {
   telegram: 'https://t.me/UnKnOwNcHanNeL',
   twitter: 'https://twitter.com/UnKnOwNcHanNeL',
   discord: 'https://discord.com/UnKnOwNcHanNeL',
-  logo1: '/Snoppy1.png',
+  logo1: '/caduceus1.png',
   logo2: '/Snoppy2.png',
   logo3: '/Snoppy3.png',
 };
